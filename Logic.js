b3ea9dd8cb8715293960a228b905c4631bd93c0d
Pallavic5 <pchedge235@gmail.com>
2023-02-02 08:02:35 +0530

    Added: Ability to create HTML5 form and media queries.

---
+++ Logic.js
@@ -0,0 +1,32 @@
+const text = document.querySelector("#text");
+      const textError = document.querySelector(".text-error");
+      text.addEventListener('input', function input() {
+        let nameRegex = RegExp("^[A-Z]{1}[a-z]{2,}$");
+        if (nameRegex.test(text.value)) textError.textContent = "";
+        else textError.textContent = "Name is Incorrect";
+      });
+
+      const email = document.querySelector('#email');
+      const emailError = document.querySelector('.email-error');
+      email.addEventListener('input', function input(){
+          let emailRegex = RegExp('^[a-zA-Z0-9]+([._+-][a-zA-Z0-9]+)*@([a-z1-9]+)([.][a-z]*)?(\.[a-z]{2,})$');
+          if(emailRegex.test(email.value)) 
+              emailError.textContent = " ";
+          else emailError.textContent = "Email is Incorrect";
+      });
+  
+      const number = document.querySelector('#number');
+        const numberError = document.querySelector('.number-error');
+        number.addEventListener('input', function input(){
+            let numberRegex = RegExp('^[6-9]{1}[0-9]{1}[1-9]{8}$');
+            if(numberRegex.test(number.value)) 
+            numberError.textContent = " ";
+            else numberError.textContent = "number is Incorrect";
+        });
+
+        const salary = document.querySelector('#salary');
+        const output = document.querySelector('.salary-output');
+        output.textContent=salary.value;
+        salary.addEventListener('input', function(){
+            output.textContent = salary.value;
+        });
